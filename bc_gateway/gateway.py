55fcac706fda72084b2cbf3e6c50fadac5b74440
Karel Blavka <karel.blavka@bigclown.com>
2017-08-06 11:24:12 +0200

    Remove setting for base topic

--- bc_gateway/gateway.py
+++ bc_gateway/gateway.py
@@ -23,7 +23,6 @@ config = {
     'mqtt': {
         'host': 'localhost',
         'port': 1883,
-        'topic': 'node',
         'username': '',
         'password': '',
         'cafile': None,
@@ -38,11 +37,11 @@ LOG_FORMAT = '%(asctime)s %(levelname)s: %(message)s'
 
 def mqtt_on_connect(client, userdata, flags, rc):
     logging.info('Connected to MQTT broker with code %s', rc)
-    client.subscribe(userdata['base_topic'] + '+/+/+/+/+')
+    client.subscribe('node/+/+/+/+/+')
 
 
 def mqtt_on_message(client, userdata, message):
-    subtopic = message.topic[len(userdata['base_topic']):]
+    subtopic = message.topic[5:]
     i = subtopic.find('/')
     node_name = subtopic[:i]
     node_id = userdata['rename'].get(node_name, None)
@@ -59,8 +58,6 @@ def mqtt_on_message(client, userdata, message):
 
 
 def run():
-    base_topic = config['mqtt']['topic'].rstrip('/') + '/'
-
     ser = serial.Serial(config['device'], timeout=3.0)
 
     logging.info('Opened serial port: %s', config['device'])
@@ -73,7 +70,7 @@ def run():
 
     rename = {v: k for k, v in config['rename'].items()}
 
-    mqttc = paho.mqtt.client.Client(userdata={'serial': ser, 'base_topic': base_topic, 'rename': rename})
+    mqttc = paho.mqtt.client.Client(userdata={'serial': ser, 'rename': rename})
     mqttc.on_connect = mqtt_on_connect
     mqttc.on_message = mqtt_on_message
     mqttc.username_pw_set(config['mqtt']['username'], config['mqtt']['password'])
@@ -102,7 +99,7 @@ def run():
                 if node_name:
                     subtopic = node_name + subtopic[i:]
 
-                mqttc.publish(base_topic + subtopic, json.dumps(talk[1], use_decimal=True), qos=1)
+                mqttc.publish("node/" + subtopic, json.dumps(talk[1], use_decimal=True), qos=1)
             except Exception:
                 logging.error('Failed to publish MQTT message: %s', line)
 
@@ -113,7 +110,6 @@ def main():
     argp.add_argument('-d', '--device', help='path to gateway serial port (default is /dev/ttyACM0)')
     argp.add_argument('-H', '--mqtt-host', help='MQTT host to connect to (default is localhost)')
     argp.add_argument('-P', '--mqtt-port', help='MQTT port to connect to (default is 1883)')
-    argp.add_argument('-t', '--mqtt-topic', help='base MQTT topic (default is node)')
     argp.add_argument('-W', '--wait', help='wait on connect or reconnect', action='store_true')
     argp.add_argument('-l', '--list', help='show list of available devices and exit', action='store_true')
     argp.add_argument('--mqtt-username', help='MQTT username')
@@ -148,7 +144,6 @@ def main():
     config['device'] = args.device if args.device else config['device']
     config['mqtt']['host'] = args.mqtt_host if args.mqtt_host else config['mqtt']['host']
     config['mqtt']['port'] = args.mqtt_port if args.mqtt_port else config['mqtt']['port']
-    config['mqtt']['topic'] = args.mqtt_topic if args.mqtt_topic else config['mqtt']['topic']
     config['mqtt']['username'] = args.mqtt_username if args.mqtt_username else config['mqtt']['username']
     config['mqtt']['password'] = args.mqtt_password if args.mqtt_password else config['mqtt']['password']
     config['mqtt']['cafile'] = args.mqtt_cafile if args.mqtt_cafile else config['mqtt']['cafile']
