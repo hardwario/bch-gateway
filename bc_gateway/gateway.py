1319c0177f28f0501c134a9292e01bf42e5dd1c6
Karel Blavka <karel.blavka@bigclown.com>
2017-09-12 23:09:19 +0200

    Reset serial buffer

--- bc_gateway/gateway.py
+++ bc_gateway/gateway.py
@@ -70,6 +70,8 @@ class Gateway:
             fcntl.flock(self.ser.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
             logging.debug('Exclusive lock on file descriptor: %d' % self.ser.fileno())
 
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
         self.ser.write(b'\n')
 
         self.write("/info/get", None)
