6d90ba68ccd9e0c67a57f4364129efbaf153bee8
Karel Blavka <karel.blavka@bigclown.com>
2018-05-23 22:28:16 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -48,5 +48,47 @@ setup(
     ''',
     long_description="""
 BigClown gateway between USB and MQTT broker.
+
+Documentation is here https://www.bigclown.com/doc/tools/bcg/
+
+
+usage: bcg [-h] [-c CONFIG] [-d DEVICE] [-H MQTT_HOST] [-P MQTT_PORT]
+           [--no-wait] [--mqtt-username MQTT_USERNAME]
+           [--mqtt-password MQTT_PASSWORD] [--mqtt-cafile MQTT_CAFILE]
+           [--mqtt-certfile MQTT_CERTFILE] [--mqtt-keyfile MQTT_KEYFILE] [-D]
+           [-v]
+           COMMAND ...
+
+BigClown gateway between USB serial port and MQTT broker
+
+positional arguments:
+  COMMAND
+    devices             show devices
+    help                show help
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        path to configuration file (YAML format)
+  -d DEVICE, --device DEVICE
+                        device
+  -H MQTT_HOST, --mqtt-host MQTT_HOST
+                        MQTT host to connect to (default is localhost)
+  -P MQTT_PORT, --mqtt-port MQTT_PORT
+                        MQTT port to connect to (default is 1883)
+  --no-wait             no wait on connect or reconnect serial port
+  --mqtt-username MQTT_USERNAME
+                        MQTT username
+  --mqtt-password MQTT_PASSWORD
+                        MQTT password
+  --mqtt-cafile MQTT_CAFILE
+                        MQTT cafile
+  --mqtt-certfile MQTT_CERTFILE
+                        MQTT certfile
+  --mqtt-keyfile MQTT_KEYFILE
+                        MQTT keyfile
+  -D, --debug           print debug messages
+  -v, --version         show program's version number and exit
+
 """
 )
